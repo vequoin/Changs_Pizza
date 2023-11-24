757955c8a4f25c48ca153e350d81c1cae23e68d1
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-11-24 03:25:15 -0500

    Update Order.java

--- src/main/java/com/example/ru_pizza/model/Order.java
+++ src/main/java/com/example/ru_pizza/model/Order.java
@@ -1,4 +1,45 @@
 package com.example.ru_pizza.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private static int nextOrderId = 1; // Static counter for unique order IDs
+    private final int orderId;
+    private final List<Pizza> pizzas;
+    private double totalAmount; // Total amount before tax
+
+    public Order() {
+        this.orderId = nextOrderId++;
+        this.pizzas = new ArrayList<>();
+        this.totalAmount = 0.0;
+    }
+
+    public void addPizza(Pizza pizza) {
+        pizzas.add(pizza);
+        totalAmount += pizza.price();
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public List<Pizza> getPizzas() {
+        return new ArrayList<>(pizzas);
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public double calculateTax() {
+        final double TAX_RATE = 0.06625; 
+        return totalAmount * TAX_RATE;
+    }
+
+    public double getOrderTotal() {
+        return totalAmount + calculateTax();
+    }
+
+
 }
