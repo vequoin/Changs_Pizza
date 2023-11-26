563e679a19f7643f15cc19972aaf710eb1df6bd6
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-11-26 07:03:16 -0500

    Update Seafood.java

--- src/main/java/com/example/ru_pizza/model/Seafood.java
+++ src/main/java/com/example/ru_pizza/model/Seafood.java
@@ -19,6 +19,18 @@ public class Seafood extends Pizza{
 
     @Override
     public double price() {
-        return 17.99;
+        double price = 17.99;
+        switch (size){
+            case SMALL -> price += 0.0;
+            case MEDIUM -> price += 2.00;
+            case LARGE -> price += 4.00;
+        }
+        if(isExtraCheese()){
+            price += 1;
+        }
+        if(isExtraSauce()){
+            price += 1;
+        }
+        return price;
     }
 }
