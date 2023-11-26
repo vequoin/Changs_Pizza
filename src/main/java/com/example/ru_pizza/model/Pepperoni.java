f4c38b612a32f2ed17929bbdf52fba6795a34b3f
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-11-26 07:01:45 -0500

    Update Pepperoni.java

--- src/main/java/com/example/ru_pizza/model/Pepperoni.java
+++ src/main/java/com/example/ru_pizza/model/Pepperoni.java
@@ -11,6 +11,19 @@ public class Pepperoni extends Pizza{
 
     @Override
     public double price() {
-        return 10.99;
+
+        double price = 10.99;
+        switch (size){
+            case SMALL -> price += 0.0;
+            case MEDIUM -> price += 2.00;
+            case LARGE -> price += 4.00;
+        }
+        if(isExtraCheese()){
+            price += 1;
+        }
+        if(isExtraSauce()){
+            price += 1;
+        }
+        return price;
     }
 }
