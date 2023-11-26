a857654b2085cf3c3ae76d89d1f12f99d96cbe7d
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-11-26 07:55:31 -0500

    Update StoreOrder.java

--- src/main/java/com/example/ru_pizza/model/StoreOrder.java
+++ src/main/java/com/example/ru_pizza/model/StoreOrder.java
@@ -1,22 +1,49 @@
 package com.example.ru_pizza.model;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class StoreOrder {
-    private final List<Order> orders;
+    private static int nextOrderNumber = 1;
+    private List<Order> orders;
 
     public StoreOrder() {
         orders = new ArrayList<>();
     }
 
-    public List<Order> getOrders() {
-        return orders;
-    }
-
+    // Method to add an order
     public void addOrder(Order order) {
         orders.add(order);
     }
 
+    // Method to get all orders
+    public List<Order> getOrders() {
+        return orders;
+    }
 
+
+    public void exportToFile(String filename) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for (Order order : orders) {
+                writer.write("Order Number: " + order.getOrderId() + "\n");
+                writer.write("Order Details: \n");
+                for (Pizza pizza : order.getPizzas()) {
+                    writer.write(pizza.toString() + "\n");
+                }
+                writer.write("Total Amount: " + order.getTotalAmount() + "\n");
+                writer.write("Sales Tax: " + order.calculateTax() + "\n");
+                writer.write("Order Total: " + order.getOrderTotal() + "\n\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to generate the next order number
+    public static int getNextOrderNumber() {
+        return nextOrderNumber++;
+    }
 }
