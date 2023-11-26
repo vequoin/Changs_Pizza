9a1d9be5d13be03b25c8c99c55ad0ff6a1351ce8
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-11-26 07:03:49 -0500

    Update StoreOrder.java

--- src/main/java/com/example/ru_pizza/model/StoreOrder.java
+++ src/main/java/com/example/ru_pizza/model/StoreOrder.java
@@ -1,5 +1,22 @@
 package com.example.ru_pizza.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StoreOrder {
+    private final List<Order> orders;
+
+    public StoreOrder() {
+        orders = new ArrayList<>();
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
 
 }
